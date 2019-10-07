c3da5c4a8762fc56a93766e4f756834f77783c95
Fiorela <fbasiliomanrique@gmail.com>
2019-10-07 16:17:25 -0500

    Modificando el validator y agregando estilos

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const numero = document.getElementById('numero');
 const tarjeta = document.getElementById('tarjeta');
 const validar = document.getElementById('validar');
 
-validar.addEventListener('click', () => {
+validar.addEventListener('click', (e) => {
+    e.preventDefault();
     isValid(numero);
 });
 
--- src/validator.js
+++ src/validator.js
@@ -1,38 +1,38 @@
 export const isValid = (creditCardNumber) => {
   let suma = 0;
-    let newArray = [];
+  let newArray = [];
 
-    const reverseNumero = (numero.value.split(''));
-    console.log(reverseNumero);
+  const reverseNumero = (numero.value.split(''));
+  console.log(reverseNumero);
 
-    for (let i = 0; i < reverseNumero.length; i++) {
-        if (i % 2 === 0) {
-            newArray.push(reverseNumero[i] * 2);
-        } else {
-            newArray.push(parseInt(reverseNumero[i]));
-        }
+  for (let i = 0; i < reverseNumero.length; i++) {
+    if (i % 2 === 0) {
+      newArray.push(reverseNumero[i] * 2);
+    } else {
+      newArray.push(parseInt(reverseNumero[i]));
     }
-    console.log(newArray);
+  }
+  console.log(newArray);
 
-    // SUMA
+  // SUMA
 
-    for (let i = 0; i < newArray.length; i++) {
-        if (newArray[i] >= 10) {
-            const numberCadena = newArray[i].toString();
-            for (let i = 0; i < numberCadena.length; i++) {
-                suma += parseInt(numberCadena[i]);
-            }
-
-        } else {
-            suma += parseInt(newArray[i]);
-        }
-        console.log(suma);
-    }
-    if (suma % 10 === 0) {
-        alert('valida')
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] >= 10) {
+      const numberCadena = newArray[i].toString();
+      for (let i = 0; i < numberCadena.length; i++) {
+        suma += parseInt(numberCadena[i]);
+      }
 
     } else {
-        alert('no valida')
+      suma += parseInt(newArray[i]);
     }
-  };
+    console.log(suma);
+  }
+  if (suma % 10 === 0) {
+    alert('valida')
+
+  } else {
+    alert('no valida')
+  }
+};
 
